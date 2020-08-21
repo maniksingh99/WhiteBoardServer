f3bc55b7f15ac774cdbaa36ccbb6c100cc8ed83f
maniksingh99 <maniksingh99@gmail.com>
2020-08-21 19:32:07 +0530

    whiteboard server

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+
+const http = require('http').createServer(app);
+
+//socket enabled server
+const io = require('socket.io')(http);
+
+app.get("/", function (req, res) {
+    res.end("<h1>Welcome to home Page</h1>")
+})
+
+io.on("connection",function(socket){
+    console.log("New client connected");
+    console.log(socket.id);
+    socket.on("color",function(color){
+        socket.broadcast.emit('colorchange',color);
+    })
+    socket.on("md",function(point){
+        socket.broadcast.emit("onmd",point);
+    })
+    socket.on("mm",function(point){
+        socket.broadcast.emit("onmm",point);
+    })
+})
+
+let port = process.env.PORT || 3000;
+http.listen(port,function(){
+    console.log("Server started at port 3000");
+})
